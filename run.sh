fb0d6fe378b0a5da0589ac7c07449c8d7fd84735
wanghan1110 <wanghan1110@gmail.com>
2017-04-05 23:19:30 -0400

    Update python file name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo The program takes around 2 minutes. Thanks for your patience!
-python3 ./src/InsightChallenge_WangHan.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
+echo The program takes around a few minutes. Thanks for your patience!
+python3 ./src/InsightChallenge_v1_WangHan.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
